1f5ab7d451b0325621cfaca4d6a0a978ea447d7b
Rafael Ramirez Lozano <rafaelrl85@hotmail.com>
2017-10-15 20:32:09 -0500

    Config Client Spring Cloud

--- src/main/java/com/cloud/lab3client/Lab3ClientApplication.java
+++ src/main/java/com/cloud/lab3client/Lab3ClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab3ClientApplication {
 
+	//Run this on localhost 8002, and go to the url defined on the controller.
 	public static void main(String[] args) {
 		SpringApplication.run(Lab3ClientApplication.class, args);
 	}
